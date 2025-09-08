aa748725275f3fc62b5e199d1c7d388298308a80
Katia Lopes-Gilbert <katia.lopes-gilbert@lumatahealth.com>
2025-09-08 09:48:29 -0700

    Starting data profile folder.

---
+++ data-overview/ydata-profile/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+df = pd.read_excel('G:\Shared drives\Data and Research Team\Ad Hoc Requests\RCMG Test Data\Test File.xlsx', skiprows=5, usecols=lambda x: x not in ['Unnamed: 0'])
+
+profile = ProfileReport(df, config_file="lumata_config.yaml")
+
+profile.to_file("Profile Report.html")
+
+print("Profiling report generated and saved as 'Profiling Report.html'")
+
+
