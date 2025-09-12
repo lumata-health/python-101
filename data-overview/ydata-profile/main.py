c5698df622de161c6d88106e69ace93e6a9b00aa
Katia Lopes-Gilbert <katia.lopes-gilbert@lumatahealth.com>
2025-09-12 13:44:29 -0700

    Test commit.

--- data-overview/ydata-profile/main.py
+++ data-overview/ydata-profile/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-df = pd.read_excel('G:\Shared drives\Data and Research Team\Ad Hoc Requests\RCMG Test Data\Test File.xlsx', skiprows=5, usecols=lambda x: x not in ['Unnamed: 0'])
+df = pd.read_excel('G:\Shared drives\Data and Research Team\Ad Hoc Requests\RCMG Test Data\Test File.xlsx',
+                   skiprows=5, usecols=lambda x: x not in ['Unnamed: 0'])
 
 profile = ProfileReport(df, config_file="lumata_config.yaml")
 
@@ -10,3 +11,4 @@ profile.to_file("Profile Report.html")
 print("Profiling report generated and saved as 'Profiling Report.html'")
 
 
+print('Hello team!!!')
